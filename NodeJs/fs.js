87ce7761f6df909c5263dd5f6b9bd70a3e435651
lekhraj tank <lekhrajtak229@gmail.com>
2024-08-29 21:33:09 +0530

    NodeCommit

---
+++ NodeJs/fs.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+
+// fs.mkdirSync("./built-in")
+
+// fs.rmdirSync("./built-in")
+
+// fs.mkdirSync("./local");
+// fs.mkdirSync("./local/user");
+fs.mkdirSync("../local/users");
