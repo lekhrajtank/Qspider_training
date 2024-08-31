904a92dd020c41782b13bfed01cadd9481cb0d92
lekhrajtank <119410412+lekhrajtank@users.noreply.github.com>
2024-08-31 18:45:12 +0530

    Delete

--- NodeJs/fs.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-// fs.mkdirSync("./built-in")
-
-// fs.rmdirSync("./built-in")
-
-// fs.mkdirSync("./local");
-// fs.mkdirSync("./local/user");
-fs.mkdirSync("../local/users");
